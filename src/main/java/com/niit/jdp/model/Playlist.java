79e3958072f0bb6b230790569658dfad9fd8fb08
@ajaydixit598 <ajaydixit598@gmail.com>
2022-12-01 13:24:23 +0530

    created constructor ,getter and setter in playlist class

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -10,4 +10,38 @@ public class Playlist {
     private int playlistId;
     private String playlistName;
     private int SongId;
+
+    public Playlist() {
+    }
+
+    public Playlist(int playlistId, String playlistName, int songId) {
+        this.playlistId = playlistId;
+        this.playlistName = playlistName;
+        SongId = songId;
+    }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public int getSongId() {
+        return SongId;
+    }
+
+    public void setSongId(int songId) {
+        SongId = songId;
+    }
+
 }
