bf40939473a00008a96f035bbc803c4f30991e79
@ajaydixit598 <ajaydixit598@gmail.com>
2022-12-01 09:21:04 +0530

    created constructor and getter setter for song class

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -12,4 +12,45 @@ public class Song {
     private String genre;
     private String artistName;
 
+    public Song() {
+    }
+
+    public Song(int songId, String songName, String genre, String artistName) {
+        this.songId = songId;
+        this.songName = songName;
+        this.genre = genre;
+        this.artistName = artistName;
+    }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
 }
