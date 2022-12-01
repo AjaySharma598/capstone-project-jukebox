7f7a0e7463b9a26f3341f1dab60f37170a972bc1
@ajaydixit598 <ajaydixit598@gmail.com>
2022-12-01 10:09:24 +0530

    created connection to song database

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -6,11 +6,28 @@
 
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseService {
 
-    private static final String URL = " ";
-    private static final String USER_NAME = " ";
-    private static final String PASSWORD = " ";
+    private static final String URL = "jdbc:mysql://localhost:3306/songdatabase ";
+    private static final String USERNAME = "root ";
+    private static final String PASSWORD = "96169616 ";
+
+    Connection connection;
+
+    public void Connect() throws ClassNotFoundException, SQLException {
+
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+    }
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        Connect();
+        return connection;
+    }
 
 
 }
