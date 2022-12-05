70086305d03e178a6c150e26bb3b48f930ad800a
@ajaydixit598 <ajaydixit598@gmail.com>
2022-12-05 19:15:37 +0530

    created playlist not found exception class

---
+++ src/main/java/com/niit/jdp/exception/PlaylistNotFound.java
@@ -0,0 +1,10 @@
+/*
+ * Author:Ajay Sharma
+ * Date : 05-12-2022
+ *Created With : Intellij IDEA Community Edition
+ */
+
+package com.niit.jdp.exception;
+
+public class PlaylistNotFound extends Exception {
+}
